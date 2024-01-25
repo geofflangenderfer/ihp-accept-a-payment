12541277b947b8f06d5d3a992cb3ad1d0d7c85a9
Geoff Langenderfer <geoff.langenderfer@gmail.com>
2024-01-24 19:13:14 -0800

    feat: buy button/picture on homepage
    partially done. Need to rename things.

--- Web/View/Static/Welcome.hs
+++ Web/View/Static/Welcome.hs
@@ -5,38 +5,29 @@ data WelcomeView = WelcomeView
 
 instance View WelcomeView where
     html WelcomeView = [hsx|
-         <div style="background-color: #657b83; padding: 2rem; color:hsla(196, 13%, 96%, 1); border-radius: 4px">
-              <div style="max-width: 800px; margin-left: auto; margin-right: auto">
-                  <h1 style="margin-bottom: 2rem; font-size: 2rem; font-weight: 300; border-bottom: 1px solid white; padding-bottom: 0.25rem; border-color: hsla(196, 13%, 60%, 1)">
-                      IHP
-                  </h1>
 
-                  <h2 style="margin-top: 0; margin-bottom: 0rem; font-weight: 900; font-size: 3rem">
-                      It's working!
-                  </h2>
+        <div class="sr-root">
+          <div class="sr-main">
+            <section class="container">
+              <div>
+                <h1>Single photo</h1>
+                <h4>Purchase a Pasha original photo</h4>
 
-                  <p style="margin-top: 1rem; font-size: 1.75rem; font-weight: 600; color:hsla(196, 13%, 80%, 1)">
-                     Your new application is up and running.
-                  </p>
-
-                  <p>
-                      <a
-                          href="https://ihp.digitallyinduced.com/Slack"
-                          style="margin-top: 2rem; background-color: #268bd2; padding: 1rem; border-radius: 3px; color: hsla(205, 69%, 98%, 1); text-decoration: none; font-weight: bold; display: inline-block; box-shadow: 0 4px 6px hsla(205, 69%, 0%, 0.08); transition: box-shadow 0.2s; transition: transform 0.2s;"
-                          target="_blank"
-                      >Join our community on Slack!</a>
-                  </p>
-
-                  <a href="https://ihp.digitallyinduced.com/Guide/your-first-project.html" style="margin-top: 2rem; background-color: #268bd2; padding: 1rem; border-radius: 3px; color: hsla(205, 69%, 98%, 1); text-decoration: none; font-weight: bold; display: inline-block; box-shadow: 0 4px 6px hsla(205, 69%, 0%, 0.08);  transition: box-shadow 0.2s; transition: transform 0.2s;" target="_blank">
-                     Learn the Next Steps in the Documentation
-                  </a>
+                <div class="pasha-image">
+                  <img
+                    src="https://picsum.photos/280/320?random=4"
+                    width="140"
+                    height="160"
+                    />
+                </div>
               </div>
-         </div>
 
-         <div style="max-width: 800px; margin-left: auto; margin-right: auto; margin-top: 4rem">
-              <img src="/ihp-welcome-icon.svg" alt="/ihp-welcome-icon" style="width:100%;">
-              <p style="color: hsla(196, 13%, 50%, 1); margin-top: 4rem">
-                 You can modify this start page by making changes to "./Web/View/Static/Welcome.hs".
-              </p>
-         </div> 
+              <form action="/create-checkout-session" method="POST">
+                <button id="submit">Buy</button>
+              </form>
+            </section>
+
+            <div id="error-message"></div>
+          </div>
+        </div>
 |]
